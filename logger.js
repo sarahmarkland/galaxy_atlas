70f472ead4344529dfa04d46d59b0eb58f9f7a2f
pretzelogical <yingyangyo266@gmail.com>
2024-04-09 12:22:31 -0500

    log debug in logs/debug.log if server in dev mode

--- logger.js
+++ logger.js
@@ -3,17 +3,23 @@ import { isInDevMode } from './utils.js';
 
 const DB_DEV = isInDevMode();
 
+const loggerTransports = [
+  new transports.Console({ level: DB_DEV ? 'debug' : 'info' }), // Log to console
+  new transports.File({ filename: 'logs/error.log', level: 'error' }), // Log errors to a file
+  new transports.File({ filename: 'logs/info.log', level: 'info' }) // Log all levels to another file
+];
+
+if (DB_DEV) {
+  loggerTransports.push(new transports.File({ filename: 'logs/debug.log', level: 'debug' })); // If db in dev mode log debug
+}
+
 const logger = createLogger({
   level: 'debug', // Set the minimum log level (options: error, warn, info, http, verbose, debug, silly)
   format: format.combine(
     format.timestamp(), // Add timestamp to logs
     format.json() // JSON format for logs
   ),
-  transports: [
-    new transports.Console({ level: DB_DEV ? 'debug' : 'info' }), // Log to console
-    new transports.File({ filename: 'logs/error.log', level: 'error' }), // Log errors to a file
-    new transports.File({ filename: 'logs/info.log', level: 'info' }) // Log all levels to another file
-  ]
+  transports: loggerTransports
 });
 
 export default logger;
